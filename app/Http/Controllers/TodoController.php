03d4248fb221e227666a61f3f081ba98b7a21ee3
izzazfauzi <m.izzaz93@gmail.com>
2020-08-05 00:12:52 +0800

    Todo List MP Validation

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TodoController extends Controller
 {
@@ -16,6 +17,23 @@ class TodoController extends Controller
     }
 
     public function store(Request $request) {
+        // default validator
+//        $request->validate([
+//            'title' => 'required|max:255',
+//        ]);
+
+        //custom validator
+        $rules = [
+            'title' => 'required|max:255'
+        ];
+        $messages = [
+            'title.max' => 'Todo title should not be greater than 255 characters'
+        ];
+        $validator = Validator::make($request->all(), $rules, $messages);
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
         Todo::create($request->all());
         return redirect()->back()->with('message', 'Todo Created Successfully');
     }
--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -6,4 +6,14 @@
         {{$slot}}
         <div class="py-2 px-2 mt-2 bg-danger">{{session()->get('error')}}</div>
     @endif
+
+    @if($errors->any())
+        <div class="py-2 px-2 mt-2 bg-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 </div>
